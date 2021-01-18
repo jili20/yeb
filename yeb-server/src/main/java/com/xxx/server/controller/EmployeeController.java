9b2065f4111da4ad7bd3be9a241270d342784c7e
bing <bing_yu2001@163.com>
2021-01-18 14:19:03 +0800

    更新 删除 员工

--- yeb-server/src/main/java/com/xxx/server/controller/EmployeeController.java
+++ yeb-server/src/main/java/com/xxx/server/controller/EmployeeController.java
@@ -77,20 +77,39 @@ public class EmployeeController {
 
     @ApiOperation(value = "获取所有部门")
     @GetMapping("/deps")
-    public List<Department> getAllDepartments(){
+    public List<Department> getAllDepartments() {
         return departmentService.getAllDepartments();
     }
 
     @ApiOperation(value = "获取工号")
     @GetMapping("/maxWorkID")
-    public RespBean maxWorkID(){
+    public RespBean maxWorkID() {
         return employeeService.maxWorkID();
     }
 
     @ApiOperation(value = "添加员工")
     @PostMapping("/")
-    public RespBean addEmp(@RequestBody Employee employee){
+    public RespBean addEmp(@RequestBody Employee employee) {
         return employeeService.addEmp(employee);
+    }
+
+    @ApiOperation(value = "更新员工")
+    @PutMapping("/")
+    public RespBean updateEmp(@RequestBody Employee employee) {
+        if (employeeService.updateById(employee)) {
+            return RespBean.success("更新成功！");
+        }
+        return RespBean.error("更新失败！");
+    }
+
+    @ApiOperation(value = "删除员工")
+    @DeleteMapping("/{id}")
+    public RespBean deleteEmp(@PathVariable Integer id) {
+        if (employeeService.removeById(id)) {
+            return RespBean.success("删除成功！");
+        }
+        return RespBean.error("删除失败！");
+
     }
 }
 
